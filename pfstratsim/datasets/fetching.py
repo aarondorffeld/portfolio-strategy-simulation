d88b8b71ecc96734aaee4b3afe5c0230c0e409cd
Aaron Dorffeld <aaron.dorffeld@gmail.com>
2021-12-05 01:19:56 +0900

    MNT Some Improvements of a prices fetching method

--- pfstratsim/datasets/fetching.py
+++ pfstratsim/datasets/fetching.py
@@ -1,7 +1,7 @@
 import os
-
 import pandas as pd
 from pandas_datareader import data
+from datetime import timedelta
 
 
 def load_sample_prices(asset_name_list, start_time, end_time, interest_rate=None, **params):
@@ -42,7 +42,8 @@ def load_sample_prices(asset_name_list, start_time, end_time, interest_rate=None
     return sample_prices
 
 
-def fetch_prices(asset_name_list, start_time, end_time, interest_rate=None, kind="Close", save_dir=".", **params):
+def fetch_prices(asset_name_list, start_time, end_time=None, interest_rate=None, kind="Close", save_dir=".",
+                 is_save_each=True, is_save_all=True, save_file_name="all_assets_prices", **params):
     """Load the historical prices of the arbitrary assets.
 
     Parameters
@@ -53,7 +54,7 @@ def fetch_prices(asset_name_list, start_time, end_time, interest_rate=None, kind
     start_time : Timestamp
         The start time for the simulation.
 
-    end_time : Timestamp
+    end_time : Timestamp, default=None
         The end time for the simulation.
 
     interest_rate : float, default None
@@ -65,6 +66,15 @@ def fetch_prices(asset_name_list, start_time, end_time, interest_rate=None, kind
     save_dir : str, default "."
         The directory of the historical prices of the assets.
 
+    is_save_each : bool, default=True
+        The option of whether to save the historical prices of the assets in a file by each asset.
+
+    is_save_all : bool, default=True
+        The option of whether to save the historical prices of the assets in a file for all assets.
+
+    save_file_name : str, default "all_assets_prices"
+        The file name of the historical prices of all the assets.
+
     params : dict
         The parameters not to be used in this method but necessary just to realize the API that can call this method by
         one way.
@@ -74,17 +84,22 @@ def fetch_prices(asset_name_list, start_time, end_time, interest_rate=None, kind
     prices : DataFrame of shape (num_times, num_assets) and float
         The historical prices of the assets.
     """
+    start_time += timedelta(days=1)
     prices = pd.DataFrame()
     for asset_name in asset_name_list:
         all_data = data.DataReader(asset_name, "yahoo", start_time, end_time)
         if save_dir is not None:
             if not os.path.exists(save_dir):
                 os.makedirs(save_dir)
-            all_data.to_csv(os.path.join(save_dir, f"{asset_name}.csv"))
+            if is_save_each:
+                all_data.to_csv(os.path.join(save_dir, f"{asset_name}.csv"))
         prices[asset_name] = all_data[kind]
 
     if interest_rate is not None:
         prices = _add_cash_data(prices, interest_rate)
+
+    if is_save_all:
+        prices.to_csv(os.path.join(save_dir, f"{save_file_name}.csv"))
 
     return prices
 
